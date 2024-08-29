f94dd020c84749fef1104346efc4edbb4e131b9f
Raye W <nbness2@gmail.com>
2024-08-29 05:35:24 -0600

    Converted HarvestingAction, updated HarvestTool
    HarvestTool now has a default Animation, requires a skillId and a toolId, as well as a playerMeetsRequirements to make sure in the code itself rather than outside. Lets me consolidate outside code to inside code where it's actually relevant

---
+++ src/main/java/org/dementhium/action/HarvestingAction.kt
@@ -0,0 +1,94 @@
+package org.dementhium.action
+
+import org.dementhium.model.Item
+import org.dementhium.model.mask.Animation
+import org.dementhium.model.player.Player
+
+abstract class HarvestingAction : Action(0) {
+
+    interface HarvestTool {
+        val animation: Animation get() = Animation.RESET
+        val requiredLevel: Int
+        val requiredSkill: Int
+        val toolId: Int
+
+        fun playerMeetsRequirements(player: Player): Boolean {
+            val playerHasTool = player.inventory.contains(toolId) || player.equipment.contains(toolId)
+            val playerHasLevel = player.skills.getLevel(requiredSkill) >= requiredLevel
+            return playerHasTool && playerHasLevel
+        }
+    }
+
+    interface HarvestObject {
+        val requiredLevel: Int
+        val experience: Double
+    }
+
+    var cyclesToHarvest: Int = 0
+
+    var animationCycles = 0
+
+    var started = false
+
+    override fun execute() {
+        val player = mob.player
+        val playerLevel = player.skills.getLevel(skill)
+        if (++animationCycles >= 3) {
+            player.animate(tool.animation)
+        }
+        if (cyclesToHarvest-- > 0) {
+            return
+        }
+        if (harvestObject.requiredLevel > playerLevel) {
+            player.sendMessage(getMessage(OBJECT_LEVEL))
+            stop()
+            return
+        }
+        val playerHasTool = tool.playerMeetsRequirements(player)
+        if (playerHasTool) {
+            if (tool.requiredLevel > playerLevel) {
+                player.sendMessage(getMessage(TOOL_LEVEL))
+                stop()
+                return
+            }
+            cyclesToHarvest = cycleTime
+            if (!started) {
+                started = true
+                player.sendMessage(startMessage)
+                player.animate(tool.animation)
+                return
+            }
+            if (!player.inventory.addItem(harvestReward.id, harvestReward.amount)) {
+                stop()
+                return
+            }
+            player.skills.addXp(skill, harvestObject.experience)
+            player.sendMessage(getMessage(HARVESTED_ITEM))
+        } else {
+            player.sendMessage(getMessage(NO_TOOL))
+            stop()
+            return
+        }
+    }
+
+    abstract val harvestReward: Item
+
+    abstract val startMessage: String
+
+    abstract fun getMessage(type: Int): String
+
+    abstract val skill: Int
+
+    abstract val cycleTime: Int
+
+    abstract val tool: HarvestTool
+
+    abstract val harvestObject: HarvestObject
+
+    companion object {
+        const val TOOL_LEVEL: Int = 0
+        const val NO_TOOL: Int = 1
+        const val OBJECT_LEVEL: Int = 2
+        const val HARVESTED_ITEM: Int = 3
+    }
+}
