9ca67be77d64a96ea8872c5adf28ad25ae12824e
Raye W <nbness2@gmail.com>
2024-09-07 05:40:09 -0600

    Added TrueMap
    Helps with dealing with enums in maps and being sure they are in there, of course prone to user error but I am flawless. Please ignore any bug fixes and pretend they weren't there in the first place.

---
+++ src/main/java/org/dementhium/util/TrueMap.kt
@@ -0,0 +1,22 @@
+package org.dementhium.util
+
+import kotlin.enums.EnumEntries
+
+/**
+ * This is for enums, you will always get something back because we know it's there
+ *
+ * @author: nbness2
+ */
+class TrueMap<E: Enum<E>, V>(values: EnumEntries<E>, initializer: (E) -> V): Iterable<V> {
+
+    private val internalMap = buildMap {
+        values.forEach { enumValue ->
+            val initialized = initializer(enumValue)
+            put(enumValue, initialized)
+        }
+    }
+
+    operator fun get(key: E): V = internalMap[key]!!
+
+    override fun iterator(): Iterator<V> = internalMap.keys.sorted().map { internalMap[it]!! }.iterator()
+}
