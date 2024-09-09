f7f8b95308180d05fa31c8e4a9f02ac5918debb8
Raye W <nbness2@gmail.com>
2024-09-09 16:08:43 -0600

    You weren't supposed to see that

--- src/main/java/org/dementhium/content/misc/Drinking.kt
+++ src/main/java/org/dementhium/content/misc/Drinking.kt
@@ -22,37 +22,26 @@ object Drinking {
 
     @JvmStatic
     fun drink(player: Player, drink: Drink, slot: Int) {
-        println("${player.username} drinking $drink from $slot")
         if (player.isDead) return
-        println("notdead")
         val attr = player.getAttribute<Boolean>(potionSlurp)
-        println("canslurp?: $attr")
         if (attr == true) return
-        println("canslurp")
         val slotItem = player.inventory[slot]
-        println("item: $slotItem")
         if (slotItem.id !in drink.doses) return
-        println("found doses")
         /**None of that donator shit**/
         player.setAttribute(potionSlurp, true)
-        println("set attribute true")
         World.getWorld().submitTickable(potionDelay) {
             player.removeAttribute(potionSlurp)
             stop()
         }
-        println("sent tickable")
         player.combatState.attackDelay += 2
         player.animate(829)
-        println("animated")
         val potionName = slotItem.definition.name.lowercase()
         with(drink.skillEffect) {
             player.skills.applyEffect()
         }
-        println("applied effect")
         val nextDoseId = drink.getNextDoseId(slotItem.id)
         val nextDoseItem = Item(nextDoseId)
         player.inventory[slot] = nextDoseItem
-        player.sendMessage("You drink some of your $potionName")
         player.inventory.refresh()
     }
 }
