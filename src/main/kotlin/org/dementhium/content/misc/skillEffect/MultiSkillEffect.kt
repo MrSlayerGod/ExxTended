82d57dc2117af063126e7c138cb6a50d28462664
Raye W <nbness2@gmail.com>
2024-09-10 16:07:27 -0600

    Added complete-flattening of MultiSkillEffect inputs

--- src/main/kotlin/org/dementhium/content/misc/skillEffect/MultiSkillEffect.kt
+++ src/main/kotlin/org/dementhium/content/misc/skillEffect/MultiSkillEffect.kt
@@ -3,11 +3,11 @@ package org.dementhium.content.misc.skillEffect
 import org.dementhium.model.player.skills.Skills
 import org.dementhium.util.forEachScoped
 
-class MultiSkillEffect(
-    val skillEffects: List<SkillEffect>
-): SkillEffect {
+class MultiSkillEffect(skillEffects: List<SkillEffect>): SkillEffect {
 
-    constructor(vararg effects: SkillEffect): this(effects.map(SkillEffect::flatten))
+    constructor(vararg effects: SkillEffect): this(effects.asList())
+
+    private val skillEffects: List<SkillEffect> = skillEffects.map(SkillEffect::flatten)
 
     override fun Skills.applyEffect() {
         skillEffects.forEachScoped { applyEffect() }
