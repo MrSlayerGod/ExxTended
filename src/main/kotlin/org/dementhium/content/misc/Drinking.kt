86178eca0f32cf183a24967ab22ce00769f14909
Raye W <nbness2@gmail.com>
2024-09-10 14:09:17 -0600

    Updated Drinking to use the new stuff
    I get paid by LOC btw

--- src/main/kotlin/org/dementhium/content/misc/Drinking.kt
+++ src/main/kotlin/org/dementhium/content/misc/Drinking.kt
@@ -4,6 +4,7 @@ import org.dementhium.content.misc.drinking.Drink
 import org.dementhium.model.Item
 import org.dementhium.model.World
 import org.dementhium.model.player.Player
+import org.dementhium.util.equalsItem
 import org.dementhium.util.submitTickable
 
 object Drinking {
@@ -13,12 +14,7 @@ object Drinking {
     private val potionDelay = 2
 
     @JvmStatic
-    fun forId(drinkId: Int) = Drink.values().firstOrNull { drinkId in it.doses }
-
-    fun Drink.getNextDoseId(drinkId: Int): Int {
-        val nextIndex = doses.indexOf(drinkId) - 1
-        return doses.getOrNull(nextIndex) ?: emptyId
-    }
+    fun forId(drinkId: Int) = Drink.values().firstOrNull { drinkId in it.consumableIds }
 
     @JvmStatic
     fun drink(player: Player, drink: Drink, slot: Int) {
@@ -26,22 +22,29 @@ object Drinking {
         val attr = player.getAttribute<Boolean>(potionSlurp)
         if (attr == true) return
         val slotItem = player.inventory[slot]
-        if (slotItem.id !in drink.doses) return
-        /**None of that donator shit**/
+        val nextItem: Item = when(val nextStage = drink.consumableIds.nextStageFromId(slotItem.id)) {
+            is ConsumableStatus.IdNotContained -> return
+            is ConsumableStatus.ConsumableDepleted -> when(val type = nextStage.depletionType) {
+                is DepletionType.EmptyId -> Item(type.id)
+                DepletionType.Keep -> slotItem
+                DepletionType.Remove -> Item(-1)
+            }
+            is ConsumableStatus.ConsumableNotDepleted -> Item(nextStage.id)
+        }
         player.setAttribute(potionSlurp, true)
         World.getWorld().submitTickable(potionDelay) {
             player.removeAttribute(potionSlurp)
             stop()
         }
-        player.combatState.attackDelay += 2
+        player.combatState.attackDelay += potionDelay
         player.animate(829)
         val potionName = slotItem.definition.name.lowercase()
         with(drink.skillEffect) {
             player.skills.applyEffect()
         }
-        val nextDoseId = drink.getNextDoseId(slotItem.id)
-        val nextDoseItem = Item(nextDoseId)
-        player.inventory[slot] = nextDoseItem
+        if (!slotItem.equalsItem(nextItem)) {
+            player.inventory[slot] = nextItem
+        }
         player.inventory.refresh()
     }
 }
