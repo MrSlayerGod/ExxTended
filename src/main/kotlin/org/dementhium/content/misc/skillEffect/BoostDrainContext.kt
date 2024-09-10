e68aa8e7ea7e3352ed2d971a9bcc76d0cfaa9182
Raye W <nbness2@gmail.com>
2024-09-10 16:06:59 -0600

    Moved BoostDrainContext out of helpers

---
+++ src/main/kotlin/org/dementhium/content/misc/skillEffect/BoostDrainContext.kt
@@ -0,0 +1,10 @@
+package org.dementhium.content.misc.skillEffect
+
+import org.dementhium.model.player.skills.SkillId
+import org.dementhium.model.player.skills.Skills
+
+
+class BoostDrainContext(private val skills: Skills, private val skillId: SkillId) {
+    val currentLevel get() = with(skills) { skillId.currentLevel }
+    val maximumLevel get() = with(skills) { skillId.maximumLevel }
+}
