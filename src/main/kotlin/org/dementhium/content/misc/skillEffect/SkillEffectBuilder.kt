38ba19bb3b345d153cee5b38c7e2c49db4246fd8
Raye W <nbness2@gmail.com>
2024-09-10 16:07:36 -0600

    Finished SkilEffectBuilder

--- src/main/kotlin/org/dementhium/content/misc/skillEffect/SkillEffectBuilder.kt
+++ src/main/kotlin/org/dementhium/content/misc/skillEffect/SkillEffectBuilder.kt
@@ -1,45 +1,19 @@
 package org.dementhium.content.misc.skillEffect
 
 import org.dementhium.model.player.skills.SkillId
-import org.dementhium.model.player.skills.Skills
-import org.dementhium.util.Modify
 import org.dementhium.util.builder.GenericBuilder
-import org.dementhium.util.math.percentOf
-
-class BoostDrainContext(private val skills: Skills, private val skillId: SkillId) {
-    val currentLevel get() = with(skills) { skillId.currentLevel }
-    val maximumLevel get() = with(skills) { skillId.maximumLevel }
-}
-
-fun boost(skillId: SkillId, calculateBoost: BoostDrainContext.() -> Int) = SkillEffect {
-    val context = BoostDrainContext(this, skillId)
-    val boostAmount = context.calculateBoost()
-    val cap = skillId.maximumLevel + boostAmount
-    boostLevel(skillId, boostAmount, cap)
-}
-
-fun boost(valueToBoost: Int, flat: Int = 0, percent: Int = 0): Int = (percent percentOf valueToBoost) + flat
-
-sealed class ModifyInt(val flat: Int, val percent: Int, inline val operation: (Int, Int) -> Int): Modify<Int> {
-    final override fun modifyValue(value: Int): Int = operation(percent percentOf value, flat)
-}
-
-private class Boost(flat: Int, percent: Int): ModifyInt(flat, percent, Int::plus)
-private class Drain(flat: Int, percent: Int): ModifyInt(flat, percent, Int::minus)
-private class Restore(flat: Int, percent: Int): ModifyInt(flat, percent, Int::plus)
 
 class SkillEffectBuilder: GenericBuilder<SkillEffect>("SkillEffectBuilder") {
 
-    private class Intermediate(val modify: Modify<Int>) {
-        operator fun invoke(flat: Int, percent: Int) =
-    }
-
-    private val skillModifications: MutableMap<SkillId, Modify<Int>> = mutableMapOf()
+    private val skillEffects: MutableList<SkillEffect> = mutableListOf()
 
     sealed interface HealType {
         data object Overheals: HealType
         data object DoesntOverheal: HealType
     }
+
+    var hurt: Int = 0
+    fun hurt(newHurt: Int) = apply { hurt = newHurt }
 
     var heal: Int = 0
     var healType: HealType = HealType.DoesntOverheal
@@ -51,19 +25,33 @@ class SkillEffectBuilder: GenericBuilder<SkillEffect>("SkillEffectBuilder") {
 
     fun overheal(newHeal: Int) = heal(newHeal, HealType.Overheals)
 
-    fun SkillId.boost(flat: Int = 0, percent: Int = 0) = apply {
-        skillModifications[this] = Boost(flat, percent)
-    }
-    fun SkillId.drain(flat: Int = 0, percent: Int = 0) = apply {
-        skillModifications[this] = Drain(flat, percent)
-    }
-    fun SkillId.restore(flat: Int = 0, percent: Int = 0) = apply {
-        skillModifications[this] = Restore(flat, percent)
-    }
-
-    operator fun SkillId.unaryPlus
+    fun addEffect(skillEffect: SkillEffect) = apply { skillEffects.add(skillEffect) }
+    fun SkillId.boost(flat: Int = 0, percent: Int = 0) = addEffect(boost(this, flat, percent))
+    fun SkillId.drain(flat: Int = 0, percent: Int = 0) = addEffect(drain(this, flat, percent))
+    fun SkillId.restore(flat: Int = 0, percent: Int = 0) = addEffect(restore(this, flat, percent))
 
     override fun build(): SkillEffect {
-        if ()
+        if (heal != 0) {
+            skillEffects += SkillEffect {
+                heal(
+                    heal,
+                    maxHp = if (healType is HealType.Overheals) {
+                        heal + maxHitpoints
+                    } else {
+                        heal
+                    }
+                )
+            }
+        }
+        if (hurt != 0) {
+            skillEffects += SkillEffect { hit(hurt) }
+        }
+        val toReturn = when {
+            skillEffects.isEmpty() -> SkillEffect.Inert
+            skillEffects.size == 1 -> skillEffects[0]
+            else -> MultiSkillEffect(skillEffects.toList())
+        }
+        skillEffects.clear()
+        return toReturn
     }
 }
